1a93e2ccd932675780381ed84052c296e847373f
Vladislav Tsarenko <vadiktsarenko@mail.ru>
2020-12-02 23:12:10 +0300

    create models room and person

--- office/models.py
+++ office/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+
+class Room(models.Model):
+    number = models.PositiveIntegerField(db_index=True)
+    seats_num = models.PositiveIntegerField(
+        validators=[MaxValueValidator(30, message="Number of seats must be lower than 30"), ])
+
+    class Meta:
+        ordering = ('number',)
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ('name',)
